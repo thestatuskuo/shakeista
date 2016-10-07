4db2bead1b457781dc9fee1b1011d6a80a094106
Sophia Kuo <skuo@beachbody.com>
2016-10-06 19:15:27 -0700

    Merge JS

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,15 @@
 $( document ).ready(function() {
-<<<<<<< Updated upstream
 	buildInventoryList();
-	// Ignition.getOrders().then(function(data) {
-	// 	console.log(data);
-	// });
+    var orders = Ignition.getOrders();
+    orders.then(function(data) {
+		$.each(data, function(i, item) {
+			$( "#orders .logo-wrapper" ).after( 
+				'<div class="order"><p class="order-name">Sharie</p><p class="subhead">Order</p><div class="order-wrapper"><p class="order-title">Flavor: <span class="order-flavor">'+data[i]['Flavors[]']+'</span></p><p class="order-title">Base: <span class="order-base">'+data[i]['Base[]']+'</span></p><p class="order-title">Add-Ons: <span class="order-add-ons">'+data[i]['Add-Ins[]']+'</span></p><p class="order-title">Boosts: <span class="order-boosts">'+data[i]['Boosts[]']+'</span></p></div><p class="subhead">Special Instructions</p><textarea class="order-instructions-box">'+data[i]['note']+'</textarea></div>'
+			);
+		});
+		$("#orders .order").first().addClass('first-order');
+		$("#orders .first-order").next('#orders .order').addClass('second-order');
+    });
 
 	$("#submitMissing").click(function() {
 		Ignition.postMissingIngredients(getMissingIngredients()).then(function(data) {
@@ -58,16 +64,3 @@ function getMissingIngredients() {
 	}
 	return list;
 }
-=======
-    var orders = Ignition.getOrders();
-    orders.then(function(data) {
-		$.each(data, function(i, item) {
-			$( "#orders .logo-wrapper" ).after( 
-				'<div class="order"><p class="order-name">Sharie</p><p class="subhead">Order</p><div class="order-wrapper"><p class="order-title">Flavor: <span class="order-flavor">'+data[i]['Flavors[]']+'</span></p><p class="order-title">Base: <span class="order-base">'+data[i]['Base[]']+'</span></p><p class="order-title">Add-Ons: <span class="order-add-ons">'+data[i]['Add-Ins[]']+'</span></p><p class="order-title">Boosts: <span class="order-boosts">'+data[i]['Boosts[]']+'</span></p></div><p class="subhead">Special Instructions</p><textarea class="order-instructions-box">'+data[i]['note']+'</textarea></div>'
-			);
-		});
-		$("#orders .order").first().addClass('first-order');
-		$("#orders .first-order").next('#orders .order').addClass('second-order');
-    });
-});
->>>>>>> Stashed changes
